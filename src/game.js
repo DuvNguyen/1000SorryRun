58bb9c681e383ad9db653b07feb276270ae3a96c
Nguyen Duc Vi <nducvi.information@gmail.com>
2025-03-23 17:25:54 +0700

    again

--- src/game.js
+++ src/game.js
@@ -31,11 +31,11 @@ let gameOver = false;
 
 // Biến tần suất xuất hiện bẫy
 let obstacleSpawnRate = 3000; // tần suất ban đầu là 3 giây một phát
-const minSpawnRate = 700; // tần suất nhiều nhất là 1 giây một phát
+const minSpawnRate = 1500; // tần suất nhiều nhất là 1 giây một phát
 
 // Biến tốc độ chạy của bẫy
 let obstacleSpeed = 3;
-const maxObstacleSpeed = 5;
+const maxObstacleSpeed = 7;
 
 // Biến thắng game
 let gameWon = false;
@@ -115,7 +115,7 @@ function updateScore(){
 
 // Hàm cập nhật tốc độ chướng ngại vật dựa trên điểm số
 function updateObstacleSpeed() {
-    obstacleSpeed = 3 + Math.floor(score / 100); // Cứ mỗi 10 điểm, tốc độ tăng 1 đơn vị
+    obstacleSpeed = 3 + Math.floor(score / 200); // Cứ mỗi 10 điểm, tốc độ tăng 1 đơn vị
 }
 
 
